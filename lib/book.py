a0b0391e85dad1b33a8455f77551c44d5aeaefee
Zacharia Ndinguri <zacthuku@gmail.com>
2025-05-10 15:17:30 +0300

    labs

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python3
 
 class Book:
+    def __init__(self, title, page_count, ):
+        self.title=title
+        self.page_count = page_count
+       
+    def get_page_count(self):
+        return self._page_count
+    def set_page_count(self, page_count):
+        if isinstance(page_count, int):
+            self._page_count= page_count
+        else:
+            print("page_count must be an integer")
+    page_count=property(get_page_count, set_page_count)
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+        
     pass
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 class Shoe:
+    def __init__(self, brand, size):
+        self.brand=brand
+        self.size=size
+    @property
+    def size(self):
+        return self._size
+    @size.setter
+    def size(self, size):
+        if isinstance(size, int):
+            self._size= size
+        else:
+            print("size must be an integer")
+    def cobble(self):
+        self.condition="New"
+        print("Your shoe is as good as new!")
+    
+        
     pass
